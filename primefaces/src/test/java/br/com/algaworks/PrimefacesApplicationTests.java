bb3ae52e99e7c96dae4e9be6aa6103abdf4696c6
paulo_campelo <paulo_campelo@hst.com.br>
2020-05-03 17:29:17 -0300

    Adicionando o projet Primefaces

---
+++ primefaces/src/test/java/br/com/algaworks/PrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimefacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
